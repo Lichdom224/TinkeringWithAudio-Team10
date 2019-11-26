2b950e1dffea0b6e5a939c5d6dcea87f38283f21
CG233192 <cg233192@falmouth.ac.uk>
2019-11-26 09:55:21 +0000

    Added additional comments to Contract 3: AudioTinker.cs

--- Contract 3 - Melody Generation (Non-Diegetic Audio)/TinkeringAudio/Assets/Scripts/AudioTinker.cs
+++ Contract 3 - Melody Generation (Non-Diegetic Audio)/TinkeringAudio/Assets/Scripts/AudioTinker.cs
@@ -7,6 +7,19 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
+/*
+Contract 3 - Melody Generation (Non-Diegetic Audio)
+
+Link to GitHub repository: https://github.com/ColeGGilbert/TinkeringWithAudio-Team10
+Code and project authored by Cole Gilbert under the GPL 3.0 license
+Script template provided in Comp120 Tinkering Audio lectures was used for basic functions
+
+This script should be attatched to a gameObject with the audio source component.
+The purpose of this script is to make a melody using elements of procedural generation to control the output of tones and chords through samples, output as audio clips.
+CreateToneAudioClip() function was taken from the template for tinkering audio and modified for use inside of this project to fit the contract's needs.
+The script uses public variables for sampleDurationSeconds and octave to allow for manipulation outside of the source code. Both of these variables can be changed in the inspector.
+The script makes use of programming paradigms to create algorithms that are appropriate for use in the generation of a music track.
+*/
 
 public class AudioTinker : MonoBehaviour {
 
@@ -21,6 +34,11 @@ public class AudioTinker : MonoBehaviour {
 	// Octave affects the frequency that each note is generated as
 	// E.g A4 = Octave value of 3
 	public int octave = 2;
+
+	// sampleDurationSeconds can be changed in inspector
+	// sampleDurationSeconds affects how long each chord will last
+	// Default is set to 2 so that chord changes are not overly common so not to drown out the melody
+	public int sampleDurationSeconds = 2;
     
 
     // Start is called before the first frame update
@@ -30,7 +48,7 @@ public class AudioTinker : MonoBehaviour {
         audioSource = GetComponent<AudioSource>();
 
 		// audioClip is created using default frequency to begin with
-        audioClip = CreateToneAudioClip(freq);
+        audioClip = CreateToneAudioClip(freq, sampleDurationSeconds);
 
 		//PlayOutAudio();
 	}
@@ -78,9 +96,9 @@ public class AudioTinker : MonoBehaviour {
     
 
 	// CreateToneAudioClip() is used to create a sound wave using a passed frequency
-    private AudioClip CreateToneAudioClip(float frequency) {
-		// sampleDurationSecs determines how long the wave will play for, value 2 is chosen so that the chord changes don't drown out the melody
-        int sampleDurationSecs = 2;
+    private AudioClip CreateToneAudioClip(float frequency, int seconds) {
+		// sampleDurationSecs determines how long the wave will play for, value 2 is chosen as default so that the chord changes don't drown out the melody
+        int sampleDurationSecs = seconds;
         int sampleRate = 44100; // 44100 is default for music
         int sampleLength = sampleRate * sampleDurationSecs;
         float maxValue = 1f / 4f;
@@ -146,35 +164,35 @@ public class AudioTinker : MonoBehaviour {
 		{
 			chord = new AudioClip[3];
 
-			chord[0] = CreateToneAudioClip(Notes(-33 + (octave*12)));
-			chord[1] = CreateToneAudioClip(Notes(-29 + (octave*12)));
-			chord[2] = CreateToneAudioClip(Notes(-26 + (octave*12)));
+			chord[0] = CreateToneAudioClip(Notes(-33 + (octave*12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-29 + (octave*12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-26 + (octave*12)), sampleDurationSeconds);
 		}
 		else if(chordToMake == "G7")
 		{
 			chord = new AudioClip[4];
 
-			chord[0] = CreateToneAudioClip(Notes(-26 + (octave*12)));
-			chord[1] = CreateToneAudioClip(Notes(-34 + (octave*12)));
-			chord[2] = CreateToneAudioClip(Notes(-19 + (octave*12)));
-			chord[3] = CreateToneAudioClip(Notes(-16 + (octave*12)));
+			chord[0] = CreateToneAudioClip(Notes(-26 + (octave*12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-34 + (octave*12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-19 + (octave*12)), sampleDurationSeconds);
+			chord[3] = CreateToneAudioClip(Notes(-16 + (octave*12)), sampleDurationSeconds);
 		}
 		else if (chordToMake == "Am")
 		{
 			chord = new AudioClip[3];
 
-			chord[0] = CreateToneAudioClip(Notes(-36 + (octave*12)));
-			chord[1] = CreateToneAudioClip(Notes(-21 + (octave*12)));
-			chord[2] = CreateToneAudioClip(Notes(-17 + (octave*12)));
+			chord[0] = CreateToneAudioClip(Notes(-36 + (octave*12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-21 + (octave*12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-17 + (octave*12)), sampleDurationSeconds);
 		}
 		else if (chordToMake == "Dm6")
 		{
 			chord = new AudioClip[4];
 
-			chord[0] = CreateToneAudioClip(Notes(-31 + (octave*12)));
-			chord[1] = CreateToneAudioClip(Notes(-28 + (octave*12)));
-			chord[2] = CreateToneAudioClip(Notes(-36 + (octave*12)));
-			chord[3] = CreateToneAudioClip(Notes(-34 + (octave*12)));
+			chord[0] = CreateToneAudioClip(Notes(-31 + (octave*12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-28 + (octave*12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-36 + (octave*12)), sampleDurationSeconds);
+			chord[3] = CreateToneAudioClip(Notes(-34 + (octave*12)), sampleDurationSeconds);
 		}*/
 		#endregion
 		#region Piano-C Major
@@ -185,25 +203,25 @@ public class AudioTinker : MonoBehaviour {
 
 			// Notes() here takes the note number and returns a frequency for use in CreateToneAudioClip()
 			// '+ (octave * 12)' is used to determine the current octave that the notes are being generated in
-			chord[0] = CreateToneAudioClip(Notes(-33 + (octave * 12)));
-			chord[1] = CreateToneAudioClip(Notes(-29 + (octave * 12)));
-			chord[2] = CreateToneAudioClip(Notes(-26 + (octave * 12)));
+			chord[0] = CreateToneAudioClip(Notes(-33 + (octave * 12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-29 + (octave * 12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-26 + (octave * 12)), sampleDurationSeconds);
 		}
 		else if(chordToMake == "Fm")
 		{
 			chord = new AudioClip[3];
 
-			chord[0] = CreateToneAudioClip(Notes(-28 + (octave * 12)));
-			chord[1] = CreateToneAudioClip(Notes(-36 + (octave * 12)));
-			chord[2] = CreateToneAudioClip(Notes(-33 + (octave * 12)));
+			chord[0] = CreateToneAudioClip(Notes(-28 + (octave * 12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-36 + (octave * 12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-33 + (octave * 12)), sampleDurationSeconds);
 		}
 		else if (chordToMake == "Gm")
 		{
 			chord = new AudioClip[3];
 
-			chord[0] = CreateToneAudioClip(Notes(-26 + (octave * 12)));
-			chord[1] = CreateToneAudioClip(Notes(-34 + (octave * 12)));
-			chord[2] = CreateToneAudioClip(Notes(-31 + (octave * 12)));
+			chord[0] = CreateToneAudioClip(Notes(-26 + (octave * 12)), sampleDurationSeconds);
+			chord[1] = CreateToneAudioClip(Notes(-34 + (octave * 12)), sampleDurationSeconds);
+			chord[2] = CreateToneAudioClip(Notes(-31 + (octave * 12)), sampleDurationSeconds);
 		}
 		#endregion
 
@@ -216,7 +234,7 @@ public class AudioTinker : MonoBehaviour {
     //[Button("Save Wav file")]
     private void SaveWavFile() {
         string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
-        var audioClip = CreateToneAudioClip(1500);
+        var audioClip = CreateToneAudioClip(1500, sampleDurationSeconds);
         SaveWavUtil.Save(path, audioClip);
     }
 #endif
