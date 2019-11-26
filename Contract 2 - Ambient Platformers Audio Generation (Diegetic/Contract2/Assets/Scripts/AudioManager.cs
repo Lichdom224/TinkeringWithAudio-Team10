e2c2e6cff85caa2b5ebd97ee6063f9f2f8d52d9b
Lichdom224 <56161281+Lichdom224@users.noreply.github.com>
2019-11-26 07:19:13 +0000

    Progress towards the project. Not yet working.

--- Contract 2 - Ambient Platformers Audio Generation (Diegetic/Contract2/Assets/Scripts/AudioManager.cs
+++ Contract 2 - Ambient Platformers Audio Generation (Diegetic/Contract2/Assets/Scripts/AudioManager.cs
@@ -1,33 +1,52 @@
 ﻿using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
 
+    //Set the sound array
     public Sound[] sounds;
 
+    //Boolian for if the sound will be active
+    public bool IsActive;
+
+    //Set public Variables for editing the sound
     public float Volume;
     public float Frequency;
     public float Pitch;
 
+    //Get Audio File from Audio Source component
     void Awake()
     {
-        gameObject.AddComponent<AudioSource>();
+       gameObject.AddComponent<AudioSource>();
     }
 
     void Update()
     {
-        
+        AudioSource.volume = Volume;
     }
+
+    //Set and get the volume slider value to the volume
     public void VolControl(float CurrentVol)
     {
         Volume = CurrentVol;
     }
+
+    //Set and get the Frequency slider value to the Frequency
     public void FreqControl(float CurrentFreq)
     {
         Frequency = CurrentFreq;
     }
+
+    //Set and get the Pitch slider value to the Pitch
     public void PitControl(float CurrentPit)
     {
         Pitch = CurrentPit;
+    }
+
+    //Reading the toggle to see if the sound is active
+    public void ToggleSound(bool ActiveToggle)
+    {
+        IsActive = ActiveToggle;
     }
 }
---
+++ Contract 2 - Ambient Platformers Audio Generation (Diegetic/Contract2/Assets/Scripts/Sound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+// Shows this class in the array
+[System.Serializable]
+
+//Creates the sound class
+public class Sound{
+    public string Name;
+    public AudioClip Clip;
+    public float Volume;
+    public float Pitch;
+
+    [HideInInspector]
+    public AudioSource Souce;
+
+}
